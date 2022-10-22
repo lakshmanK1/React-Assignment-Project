25532a6f97c7190ec05b3f4a0df4259ef2f3f09f
lakshmanK1 <laxmankamisetty1@gmail.com>
2022-10-22 13:03:01 +0530

    added React.Fragment

--- src/App.js
+++ src/App.js
@@ -17,10 +17,10 @@ function App() {
 
 
   return (
-    <div>
+    <React.Fragment>
       <User onAddUser={addUserHandler}/>
       <UserList users={usersList}/>
-    </div>
+    </React.Fragment>
   );
 }
 
